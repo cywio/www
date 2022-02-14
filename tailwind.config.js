e84324b503d2932c2c0ae38046acbf8489b0168f
Chris Walsh <christianywalsh@gmail.com>
2022-02-13 21:08:38 -0800

    Add base Tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './lib/components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addBase, theme }) {
+			addBase({
+				h1: { fontSize: theme('fontSize.lg') },
+				h2: { fontSize: theme('fontSize.md') },
+				p: { fontSize: theme('fontSize.sm') },
+			})
+		}),
+	],
 }
